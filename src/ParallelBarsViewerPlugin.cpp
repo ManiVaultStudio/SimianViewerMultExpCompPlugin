2472da9c63523e9f71f134385fbfc91b9cf7a62e
Soumyadeep Basu <44787782+basusoumyadeep@users.noreply.github.com>
2023-03-03 11:44:43 +0100

    add minimum height

--- src/ParallelBarsViewerPlugin.cpp
+++ src/ParallelBarsViewerPlugin.cpp
@@ -35,6 +35,10 @@ ParallelBarsViewerPlugin::ParallelBarsViewerPlugin(const PluginFactory* factory)
 
 	getVisibleAction().setConnectionPermissionsFlag(ConnectionPermissionFlag::All);
 	getVisibleAction().publish("ParallelBars::PluginVisibility");
+
+	//connect(&getVisibleAction(), &ToggleAction::toggled, this, [](bool toggled) -> void {
+	//	qDebug() << __FUNCTION__ << "toggled changed to" << toggled;
+	//	});
 }
 
 ParallelBarsViewerPlugin::~ParallelBarsViewerPlugin()
